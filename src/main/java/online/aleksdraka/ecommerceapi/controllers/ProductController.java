6e5058b053c7c862663d070c82671977606c5abf
Aleksandar Drakaliyski <theall3xander@gmail.com>
2025-01-20 18:06:26 +0000

    Secure endpoints with JWT

--- src/main/java/online/aleksdraka/ecommerceapi/controllers/ProductController.java
+++ src/main/java/online/aleksdraka/ecommerceapi/controllers/ProductController.java
@@ -2,12 +2,15 @@ package online.aleksdraka.ecommerceapi.controllers;
 
 import online.aleksdraka.ecommerceapi.models.Product;
 import online.aleksdraka.ecommerceapi.services.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
     private final ProductService productService;
 
@@ -18,5 +21,12 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getProducts() {
         return productService.getAllProducts();
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/products")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        productService.addProduct(product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/online/aleksdraka/ecommerceapi/services/UserService.java
@@ -0,0 +1,25 @@
+package online.aleksdraka.ecommerceapi.services;
+
+import online.aleksdraka.ecommerceapi.models.User;
+import online.aleksdraka.ecommerceapi.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+    }
+}
